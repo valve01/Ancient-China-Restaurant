c0e0c1055fb0e1985127c1e0e8a4d15c1370ace1
Vikenty Mytko <118441831+valve01@users.noreply.github.com>
2024-01-26 20:58:40 +0300

    5

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
-import mobileNav from './modules/mobile-nav';
-mobileNav();
+// import mobileNav from './modules/mobile-nav';
+import aboutSliderSwitcher from './modules/about-slider-switcher';
+// mobileNav();
+
 import './modules/header-slider';
 import './modules/about-slider-inside';
 import './modules/about-slider-outside';
+aboutSliderSwitcher()
--- src/js/modules/about-slider-switcher.js
+++ src/js/modules/about-slider-switcher.js
@@ -1,10 +1,24 @@
-const insideBtnEl = document.querySelector('.aboutUs__inside-btn');
-const outsideBtnEl = document.querySelector('.aboutUs__outside-btn');
+function sliderSwitcher() {
+	const insideBtnEl = document.querySelector('.aboutUs__inside-btn');
+	const outsideBtnEl = document.querySelector('.aboutUs__outside-btn');
 
-const insideSlider= document.querySelector('.aboutUs__slider-inside-container')
-const outsideSlider= document.querySelector('.aboutUs__slider-outside-container')
+	const insideSlider = document.querySelector('.aboutUs__slider-inside-container');
+	const outsideSlider = document.querySelector('.aboutUs__slider-outside-container');
+	// const outsideSliderMini = document.querySelector('.aboutUs__slider-outside-mini');
 
-insideBtnEl.addEventListener('click', toggleSlider);
-const toggleSlider = () => {
-	insideSlider
-};
+		// outsideSlider.classList.add('none');
+	
+	insideBtnEl.addEventListener('click', () => {
+		outsideSlider.classList.add('visually-hidden');
+		// outsideSliderMini.classList.add('visually-hidden');
+		insideSlider.classList.remove('visually-hidden');
+
+	});
+
+	outsideBtnEl.addEventListener('click', () => {
+		insideSlider.classList.add('visually-hidden');
+		outsideSlider.classList.remove('visually-hidden');
+		// outsideSliderMini.classList.remove('visually-hidden');
+	});
+}
+export default sliderSwitcher;
