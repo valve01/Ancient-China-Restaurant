ffbbf87e0118ba84e30b24a0d84b74e30f2bd76d
Vikenty Mytko <118441831+valve01@users.noreply.github.com>
2024-02-26 21:57:22 +0300

    5

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,11 @@
 // import mobileNav from './modules/mobile-nav';
 import aboutSliderSwitcher from './modules/about-slider-switcher';
-// mobileNav();
-aboutSliderSwitcher()
+
 import './modules/header-slider';
 import './modules/about-slider-inside';
 import './modules/about-slider-outside';
 import './modules/reviews-slider';
+// import './modules/vibrate';
+
+// mobileNav();
+aboutSliderSwitcher()
---
+++ src/js/modules/vibrate.js
@@ -0,0 +1,3 @@
+// import $ from 'jquery';
+// import vibratePlug from '../jquery/jquery.vibrate'
+// vibratePlug(".aboutUs__outside-btn").vibrate();
