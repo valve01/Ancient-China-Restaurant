3b2646f25d2b6399750e8d1cd1fd28f7edc87b08
Vikenty Mytko <118441831+valve01@users.noreply.github.com>
2024-02-26 17:35:24 +0300

    в

--- src/js/modules/about-slider-switcher.js
+++ src/js/modules/about-slider-switcher.js
@@ -5,23 +5,25 @@ function sliderSwitcher() {
 	const insideSlider = document.querySelector('.aboutUs__slider-inside-container');
 	const outsideSlider = document.querySelector('.aboutUs__slider-outside-container');
 
-	// var successBool = window.navigator.vibrate(1000);
 
 
 	insideBtnEl.addEventListener('click', () => {
-		// successBool();
-		navigator.vibrate(1000)
+// включает вибрацию как в звездных войнах при нажатии на кнопку "внутри"
+		navigator.vibrate([
+			500, 110, 500, 110, 450, 110, 200, 110, 170, 40, 450, 110, 200, 110, 170, 40, 500,
+		]);
+		// Конец вибрации
 		outsideSlider.classList.add('aboutUs-slider-hidden');
 		insideSlider.classList.remove('aboutUs-slider-hidden');
-		outsideBtnEl.classList.remove('aboutUs__switcher-btn--active')
-		insideBtnEl.classList.add('aboutUs__switcher-btn--active')
+		outsideBtnEl.classList.remove('aboutUs__switcher-btn--active');
+		insideBtnEl.classList.add('aboutUs__switcher-btn--active');
 	});
 
 	outsideBtnEl.addEventListener('click', () => {
 		insideSlider.classList.add('aboutUs-slider-hidden');
 		outsideSlider.classList.remove('aboutUs-slider-hidden');
-		insideBtnEl.classList.remove('aboutUs__switcher-btn--active')
-		outsideBtnEl.classList.add('aboutUs__switcher-btn--active')
+		insideBtnEl.classList.remove('aboutUs__switcher-btn--active');
+		outsideBtnEl.classList.add('aboutUs__switcher-btn--active');
 	});
 }
 export default sliderSwitcher;
