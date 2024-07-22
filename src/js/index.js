db4e0c829af31b2d51da3038eb1570647ac2c237
Vikenty Mytko <118441831+valve01@users.noreply.github.com>
2024-07-22 11:39:24 +0300

    Сделал пока меню для скачивания

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import './modules/header-slider';
 import './modules/about-slider-inside';
 import './modules/about-slider-outside';
 import './modules/reviews-slider';
-import './modules/jqPopup';
+// import './modules/jqPopup';
 import './modules/counter';
 
 
