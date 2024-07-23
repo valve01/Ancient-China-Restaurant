0d109b5c563108920a239eb629eb31030d3da2b4
Vikenty Mytko <118441831+valve01@users.noreply.github.com>
2024-07-23 13:02:03 +0300

    Разметил мобильное меню

--- src/js/modules/counter.js
+++ src/js/modules/counter.js
@@ -1,22 +1,28 @@
-const counters = document.querySelectorAll('.dish-card__counter-block');
+const cards = document.querySelectorAll('.dish-card');
+const inCartcount = document.querySelector('.header__cart-items-qauntity');
 
-counters.forEach((counter) => {
+cards.forEach((card) => {
 	let count = 0;
-    const incrementBtn = counter.querySelector('.dish-card__plus');
-    const decrementBtn = counter.querySelector('.dish-card__minus');
+	const incrementBtn = card.querySelector('.dish-card__plus');
+	const decrementBtn = card.querySelector('.dish-card__minus');
+	const addToCartBtn = card.querySelector('.dish-card__add-to-cart-btn');
+	const cardCounter = card.querySelector('.dish-card__counter');
+	incrementBtn.addEventListener('click', (e) => {
+		e.preventDefault();
+		count++;
+		cardCounter.value = count;
+	});
 
-    incrementBtn.addEventListener('click', (e) => {
-		e.preventDefault()
-        count++;
-        counter.querySelector('.dish-card__counter').value = count;
-    });
-
-    decrementBtn.addEventListener('click', (e) => {
-		e.preventDefault()
-        if (count > 0) {
-            count--;
-            counter.querySelector('.dish-card__counter').value = count;
-        }
-    });
-    counter.querySelector('.dish-card__counter').value = count;
-})
+	decrementBtn.addEventListener('click', (e) => {
+		e.preventDefault();
+		if (count > 0) {
+			count--;
+			cardCounter.value = count;
+		}
+	});
+	addToCartBtn.addEventListener('click', (e) => {
+		e.preventDefault();
+		inCartcount.innerText = parseInt(inCartcount.innerText) + parseInt(cardCounter.value);
+	});
+	cardCounter.value = count;
+});
